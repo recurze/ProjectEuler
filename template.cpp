fab316814e7077e12ad14c62401a625807c3bf57
recurze <vrahul24999@gmail.com>
2018-12-27 22:43:15 +0530

    Add starting code for all solutions

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int solve(int n) {
+
+}
+
+int main() {
+    int t; scanf("%d", &t);
+    int n;
+    for (int tc = 0; tc < t; ++tc) {
+        scanf("%d", &n);
+        printf("%d\n", solve(n));
+    }
+}
